0dd8d829aa8c6956c6464c5ed6a89e8a968ed16e
Eergn <58009226+Eergn@users.noreply.github.com>
2021-03-13 14:19:01 -0500

    Adding Source Python  Code
    We Added Source Python Code For The Repo

---
+++ ctc.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+
+Server = input("Server Address >> ")
+Username = input("Choose a Username >> ")
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((Server,497))
+
+
+def recive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if (message=="Username? >> "):
+                client.send(Username.encode("ascii"))
+            else:
+                print(message)
+        except:
+            print("An Error occurred! ")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{Username} >> {input(f"{Username} >> ")} \n'
+        client.send(message.encode("ascii"))
+
+recive_thread = threading.Thread(target=recive)
+recive_thread.start()
+
+
+write_thread = threading.Thread(target=write())
+write_thread.start()
